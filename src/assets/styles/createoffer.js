98398395e25187f49cd9860c5939aaccc428ec9b
MyJanePress <janepress940214@outlook.com>
2019-02-03 21:30:38 -0800

    createOfferform

--- src/assets/styles/createoffer.js
+++ src/assets/styles/createoffer.js
@@ -7,6 +7,78 @@ const muiStyle = theme => ({
     },
     input: {
         display: 'none',
+    },
+    offercontent: {
+        marginTop: theme.spacing.unit * 3,
+        marginLeft: theme.spacing.unit,
+    },
+    imageLabel: {
+        [theme.breakpoints.up(1700 + theme.spacing.unit * 2)]: {
+            marginLeft: theme.spacing.unit * 3,
+        },
+        marginTop: theme.spacing.unit * 3,
+    },
+    imageLabelUpload: {
+        marginTop: theme.spacing.unit * 3,
+    },
+    title: {
+        backgroundColor: '#f1f1f5',
+        borderRadius: 10,
+        paddingLeft: 20,
+        fontWeight: 500,
+        fontSize: 16,
+    },
+    description: {
+        backgroundColor: '#f1f1f5',
+        borderRadius: 10,
+        paddingLeft: 20,
+        // height: 100,
+        marginTop: theme.spacing.unit * 2,
+    },
+    startDate: {
+        marginLeft: theme.spacing.unit * 2,
+        marginRight: theme.spacing.unit * 2,
+        fontWeight: 500,
+        backgroundColor: '#f1f1f5',
+    },
+    date: {
+        verticalAlign: 'bottom'
+    },
+    price: {
+        backgroundColor: '#f1f1f5',
+        width: '30%',
+        fontFamily: 'Roboto, sans-serif',
+        marginLeft: theme.spacing.unit * 2,
+        marginRight: theme.spacing.unit * 2,
+    },
+    formCategory: {
+        width: 200,
+        marginLeft: theme.spacing.unit * 3,
+        marginTop: theme.spacing.unit * 2,
+        backgroundColor: '#f1f1f5',
+    },
+    addrPhoneSite: {
+        backgroundColor: '#f1f1f5',
+        padding: theme.spacing.unit,
+        marginTop: theme.spacing.unit * 2,
+        borderRadius: '10px',
+    },
+    proceed: {
+        marginTop: theme.spacing.unit * 3,
+        padding: `  ${theme.spacing.unit * 2}px 
+                    ${theme.spacing.unit * 4}px
+                    ${theme.spacing.unit * 2}px
+                    ${theme.spacing.unit * 4}px
+                    `,
+        borderRadius: 10,
+        backgroundColor: '#ff9c02',
+        fontSize: 24,
+        fontWeight: 900,
+        color: '#7b3900',
+        fontFamily: [
+            'Roboto',
+            'sans-serif',
+        ],  
     }
 });
 
--- src/component/CreateOffer.js
+++ src/component/CreateOffer.js
@@ -4,12 +4,57 @@ import withStyles from '@material-ui/core/styles/withStyles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import InputBase from '@material-ui/core/InputBase';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import Checkbox from '@material-ui/core/Checkbox';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import { InputAdornment } from '@material-ui/core';
 
 import muiStyle from '../assets/styles/createoffer';
 import AddPhoto from '../assets/img/addphoto.gif';
 
+
+const options = [
+  'Category to be selected',
+  'Audi',
+  'BWM',
+];
+
 class CreateOffer extends Component {
-    
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      category: '',
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+    this.handleOpen = this.handleOpen.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleClose() {
+    this.setState({ open: false });
+  }
+
+  handleOpen() {
+    this.setState({ open: true });
+  }
+
   render() {
     const { classes } = this.props;
       return (
@@ -27,40 +72,176 @@ class CreateOffer extends Component {
                 porta. Maecenas ullamcorper vehicula ipsum non ultricies.
               </Typography>
             </Grid>
-            <Grid item container spacing={10} className={classes.offercontent}>
-              <Grid item xs={4} className={classes.offerphoto} spacing={15}>
-                <input
-                  accept="image/*"
-                  className={classes.input}
-                  id="text-button-file"
-                  multiple
-                  type="file"
-                />
-
-                {/* <Button component="img" className={classes.button}> */}
-                    <img
-                    htmlFor="text-button-file"
-                    src={AddPhoto}
-                    alt="upload"
+            <form >
+              <Grid item container spacing={16} className={classes.offercontent}>
+                <Grid item xs={4} container className={classes.offerphoto} spacing={16}>
+                  <label htmlFor="contained-button-file" className={classes.imageLabelUpload}>
+                    <input
+                      accept="image/*"
+                      className={classes.input}
+                      id="contained-button-file"
+                      multiple
+                      type="file"
                     />
-                {/* </Button>                 */}
-                <img
-                  src={AddPhoto}
-                  alt="upload"
-                />
-                <img
-                  src={AddPhoto}
-                  alt="upload"
-                />
-                <img
-                  src={AddPhoto}
-                  alt="upload"
-                />
+                    <img
+                      src={AddPhoto}
+                      alt="upload"
+                    />
+                  </label>
+                  <label htmlFor="contained-button-file" className={classes.imageLabel}>
+                    <input
+                      accept="image/*"
+                      className={classes.input}
+                      id="contained-button-file"
+                      multiple
+                      type="file"
+                    />
+                    <img
+                      src={AddPhoto}
+                      alt="upload"
+                    />
+                  </label>
+                  <label htmlFor="contained-button-file" className={classes.imageLabelUpload}>
+                    <input
+                      accept="image/*"
+                      className={classes.input}
+                      id="contained-button-file"
+                      multiple
+                      type="file"
+                    />
+                    <img
+                      src={AddPhoto}
+                      alt="upload"
+                    />
+                  </label>
+                  <label htmlFor="contained-button-file" className={classes.imageLabel}>
+                    <input
+                      accept="image/*"
+                      className={classes.input}
+                      id="contained-button-file"
+                      multiple
+                      type="file"
+                    />
+                    <img
+                      src={AddPhoto}
+                      alt="upload"
+                    />
+                  </label>
+                </Grid>
+                <Grid item xs={7} className={classes.offerdesc}>
+                  <InputBase 
+                    className={classes.title}
+                    placeholder="Title"
+                    fullWidth
+                    onChange={this.handleChange}
+                  >
+                  </InputBase>
+                  <InputBase 
+                    className={classes.description}
+                    placeholder="Description"
+                    rows='10'
+                    fullWidth
+                    multiline
+                    onChange={this.handleChange}
+                  >
+                  </InputBase>
+                  <Typography align='right' variant="overline">
+                    30 - 100
+                  </Typography>
+                  <span className={classes.date}>Choose Date:</span>
+                  <TextField
+                  id="date"
+                  type="date"
+                  className={classes.startDate}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  >
+                  </TextField>
+                  <span className={classes.date}>To:</span>
+                  <TextField
+                    id="date"
+                    type="date"
+                    
+                    className={classes.startDate}
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                  >
+                  </TextField>
+                  <FormControlLabel
+                      control={<Checkbox value="remember" color="primary" />}
+                      label="Don't show a date"
+                  />
+                  <Grid>
+                    <span className={classes.date}>Full Price:</span>
+                    <TextField
+                      className={classes.price}
+                      InputProps={{
+                        startAdornment:<InputAdornment position="start">$</InputAdornment>
+                      }}
+                    >
+                    </TextField>
+                    <span className={classes.date}>Discount:</span>
+                    <TextField
+                      className={classes.price}
+                      InputProps={{
+                        startAdornment:<InputAdornment position="start">%</InputAdornment>
+                      }}
+                    >
+                    </TextField>
+                  </Grid>
+                  <Grid>
+                    <span className={classes.date}>Select the Category</span>
+                    <FormControl className={classes.formCategory}>
+                      <InputLabel htmlFor="category-to-be-selected">Category to be selected</InputLabel>
+                      <Select
+                        open={this.state.open}
+                        onClose={this.handleClose}
+                        onOpen={this.handleOpen}
+                        value={this.state.category}
+                        onChange={this.handleChange}
+                        inputProps={{
+                          name: 'category',
+                          id: 'category-to-be-selected'
+                        }}
+                      >
+                        <MenuItem value={10}>Audi</MenuItem>
+                        <MenuItem value={20}>BWM</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Grid>
+                  <InputBase 
+                    className={classes.addrPhoneSite}
+                    placeholder="Address"
+                    fullWidth
+                    onChange={this.handleChange}
+                  >
+                  </InputBase>
+                  <InputBase 
+                      className={classes.addrPhoneSite}
+                      placeholder="Phone"
+                      fullWidth
+                      onChange={this.handleChange}
+                    >
+                  </InputBase>
+                  <InputBase 
+                      className={classes.addrPhoneSite}
+                      placeholder="Site"
+                      fullWidth
+                      onChange={this.handleChange}
+                    >
+                  </InputBase>
+                  <Button
+                      type="submit"
+                      variant="contained"
+                      className={classes.proceed}
+                    >
+                      Proceed
+                  </Button>
+                </Grid>  
               </Grid>
-              <Grid item xs={8} className={classes.offerdesc}>
-                    8
-              </Grid>
-            </Grid>
+            </form>
           </Grid>
         </Paper>
       );
